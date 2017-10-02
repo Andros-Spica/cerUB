658e628527b385dd99c24afed3d2636f64d9dc24
Andros-Spica <andros.spica@gmail.com>
2017-10-02 19:51:55 +0200

    add less or equal symbol to tests result print.

--- R/test_groups.R
+++ R/test_groups.R
@@ -37,17 +37,37 @@ test_groups <- function(distMatrix, groups){
 
   x$text <- function(tests){
 
+    equalsOrLess <- function(pvalue) {
+
+      pvalue = as.character(round(pvalue, 3))
+      symbol = paste("\n                      (p = ", pvalue, ")", sep = "")
+
+      if (pvalue <= 0.001)
+        symbol = expression(paste("\n                      (p ",
+                                  ""<="", " 0.001", ")",
+                                  sep = ""))
+
+      return(symbol)
+    }
+
     permanova.F = as.character(round(tests$permanova$aov.tab$F.Model[1], 3))
     permanova.p_value = as.character(round(tests$permanova$aov.tab$"Pr(>F)"[1], 3))
+    permanova.equals = equalsOrLess(tests$permanova$aov.tab$"Pr(>F)"[1])
     permanova.rSquared = as.character(round(tests$permanova$aov.tab$R2[1], 3))
     permdisp2.F = as.character(round(tests$permdisp2$tab$F[1], 3))
     permdisp2.p_value = as.character(round(tests$permdisp2$tab$"Pr(>F)"[1], 3))
-    text <- list(paste("PERMANOVA:\n   F = ", permanova.F,
-                       " (p = ", permanova.p_value, ")", sep = ""),
+    permdisp2.equals = equalsOrLess(tests$permdisp2$tab$"Pr(>F)"[1])
+    text <- list(c(paste("PERMANOVA:\n   F = ", permanova.F, sep = ""),
+                   permanova.equals),
                  c(expression(paste("   ", R^2, " = ", sep = "")),
-                   paste("            ", permanova.rSquared, sep = "")),
-                 paste("PERMDISP2:\n   F = ", permdisp2.F,
-                       " (p = ", permdisp2.p_value,")", sep = ""))
+                   paste("            ", permanova.rSquared,
+                         sep = "")),
+                 c(paste("PERMDISP2:\n   F = ", permdisp2.F, sep = ""),
+                       permdisp2.equals)
+    )
+    #" (p ", permanova.equals, " ", permanova.p_value, ")",
+    #" (p ", permdisp2.equals, " ", permdisp2.p_value,")",
+
 
     return(text)
   }
