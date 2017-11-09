1069aa64b8ccc13367f501a39b6f3652f9c70443
Andros-Spica <andros.spica@gmail.com>
2017-11-09 12:57:03 +0100

    Added exhaustive checks in clean_and_format. Simplified default arguments.

--- R/clean_and_format.R
+++ R/clean_and_format.R
@@ -7,7 +7,7 @@
 #' @param data Data frame, a data frame to be prepared for applying cerUB protocols.
 #' @param categorical_columns Character/Numeric, vector with the names/indexes of the categorical variables.
 #' @param numerical_columns Character/Numeric, vector with the names/indexes of the numeric variables.
-#' @param completion_variable Character, vector with two elements (name, value) referencing the column that indicates wheter observations (rows) are completed.
+#' @param completion_variable Character, vector with two elements (name, value) referencing the column that indicates wheter observations (rows) are completed. For instance, \code{c("isCompleted", "yes")}.
 #' @param as_na Character, vector that specifies values to be considered as NA.
 #' @param method Character, method to be used in for replacing NA, if any (\code{\link[cerUB]{replace_na}}).
 #' @param columns_to_exclude Character/Numeric, vector with the names/indexes of columns to exclude.
@@ -40,34 +40,52 @@
 #'
 #' @export
 clean_and_format <- function(data,
-                             categorical_columns = c(),
-                             numerical_columns = c(),
-                             completion_variable = c("CHARAC", "complete"),
-                             as_na = c(NULL),
-                             method = "random",
-                             columns_to_exclude = c("variableNameToExclude"),
-                             rows_to_exclude = c("outlierObservation")) {
+                             categorical_columns = NULL,
+                             numerical_columns = NULL,
+                             completion_variable = NULL,
+                             as_na = NULL,
+                             method = NULL,
+                             columns_to_exclude = NULL,
+                             rows_to_exclude = NULL) {
 
   # "data" is a data frame
   # "complete" is the boolean indicating if output needs to have the petrological characterization complete
 
   # format numeric data
   if (!is.null(numerical_columns)) {
-    for (v in numerical_columns) {
-      data[, v] <- as.numeric(as.character(data[, v]))
+
+    numColNames <- numerical_columns
+    if (is.numeric(numerical_columns))
+      numColNames <- names(dt)[numerical_columns]
+
+    if (all(numColNames %in% names(data))) {
+      for (v in numColNames) {
+        data[, v] <- as.numeric(as.character(data[, v]))
+      }
+    } else {
+      warning("Not all numerical variables were found. Please specify the exact name of numerical index of the columns in the data frame.")
     }
   }
 
   # format categorical data
   if (!is.null(categorical_columns)) {
-    for (v in categorical_columns) {
-      data[, v] <- factor(data[, v])
+
+    catColNames <- categorical_columns
+    if (is.numeric(categorical_columns))
+      catColNames <- names(dt)[categorical_columns]
+
+    if (all(catColNames %in% names(data))) {
+      for (v in catColNames) {
+        data[, v] <- factor(data[, v])
+      }
+    } else {
+      warning("Not all categorical variables were found. Please specify the exact name of numerical index of the columns in the data frame.")
     }
   }
 
   # filter incomplete characterization (only if the variable is present)
   if (!is.null(completion_variable)) {
-    if (!is.null(data[, completion_variable[1]])) {
+    if (completion_variable[1] %in% names(data)) {
       data <- subset(data,
                      data[, completion_variable[1]] == completion_variable[2])
     } else {
@@ -77,34 +95,67 @@ clean_and_format <- function(data,
 
   # filter observations (rows)
   if (!is.null(rows_to_exclude)) {
-    for (i in 1:length(rows_to_exclude)){
-      data <- subset(data,
-                     row.names(data) != rows_to_exclude[i])
+
+    rowNamesToExclude <- rows_to_exclude
+    if (is.numeric(rows_to_exclude))
+      rowNamesToExclude <- names(dt)[rows_to_exclude]
+
+    if (length(rowNamesToExclude) > 0 &
+        all(rowNamesToExclude %in% row.names(data))) {
+      for (i in 1:length(rowNamesToExclude)){
+        data <- subset(data,
+                       row.names(data) != rowNamesToExclude[i])
+      }
+    } else {
+      warning("Not all rows to exclude were found. Please specify the exact name of numerical index of the columns in the data frame.")
     }
   }
 
   # replace Nas
-  ## categorical data
-  if (!is.null(categorical_columns)) {
-    for (v in categorical_columns){
-      data[, v] <- replace_na(data[, v],
-                              as_na = as_na,
-                              method = method)
-    }
-  }
+  if (!is.null(as_na) | !is.null(method)) {
+    ## categorical data
+    if (!is.null(categorical_columns)) {
 
-  ## numeric data
-  if (!is.null(numerical_columns)) {
-    for (v in numerical_columns){
-      data[, v] <- replace_na(data[, v],
-                              method = method)
+      catColNames <- categorical_columns
+      if (is.numeric(categorical_columns))
+        catColNames <- names(dt)[categorical_columns]
+
+      if (all(catColNames %in% names(data))) {
+        for (v in catColNames){
+          data[, v] <- replace_na(data[, v],
+                                  as_na = as_na,
+                                  method = method)
+        }
+      }
+    }
+
+    ## numeric data
+    if (!is.null(numerical_columns)) {
+
+      numColNames <- numerical_columns
+      if (is.numeric(numerical_columns))
+        numColNames <- names(dt)[numerical_columns]
+
+      if (all(numColNames %in% names(data))) {
+        for (v in numColNames){
+          data[, v] <- replace_na(data[, v],
+                                  method = method)
+        }
+      }
     }
   }
 
   # exclude variables in "columns_to_exclude"
   if (!is.null(columns_to_exclude)) {
-    for (i in columns_to_exclude){
-      data <- data[, names(data) != i]
+
+    colNamesToExclude <- columns_to_exclude
+    if (is.numeric(columns_to_exclude))
+      colNamesToExclude <- names(dt)[columns_to_exclude]
+
+    if (all(colNamesToExclude %in% names(data))) {
+      for (i in colNamesToExclude){
+        data <- data[, names(data) != i]
+      }
     }
   }
 
